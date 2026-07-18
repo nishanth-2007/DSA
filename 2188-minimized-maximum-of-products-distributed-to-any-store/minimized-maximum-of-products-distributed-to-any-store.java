894471360739d359b0db6320a9cd56c20d4a2d0d
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-07-18 21:58:34 +0530

    Time: 42 ms (50.03%) | Memory: 81.2 MB (32.86%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPossible(int quantities[],int n,int max){
+        for(int i=0;i<quantities.length;i++ ) {
+            
+            int ans=quantities[i]/max;
+            if(quantities[i]%max!=0) ans++;
+            n-=ans;
+            if(n<0) return false;
+        }
+        return true;
+
+    }
+    public int minimizedMaximum(int n, int[] quantities) {
+        int l=1;
+        int r=(int)Math.pow(10,5);
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(quantities,n,mid)) r=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+}
