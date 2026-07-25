f1694e1a867c00bc8e6a91bf2b2ccc687b20a9ea
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-07-25 19:43:07 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (92.77%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int n) {
+        int first=0,second=0;
+        while(n>0){
+            int last=n%10;
+            if(last>first){
+                second=first;
+                first=last;
+            }
+            else if(last>second) second=last;
+            n/=10;
+        }
+        return first*second;
+    }
+}
