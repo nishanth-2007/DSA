214d8cce4706df1d1b6ecc322264864b46361c9e
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-08-07 19:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (14.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,29 @@
 class Solution {
+    public int lowerBound(int[] nums,int target){
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target) high=mid-1;
+            else low=mid+1;
+        }
+        if(low>=nums.length) return -1;
+        if(nums[low]!=target) return -1;
+        return low;
+    }
+    public int upperBound(int[] nums,int target){
+        int low=0, high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        if(high<0) return -1;
+        if(nums[high]!=target) return -1;
+        return high;
+    }
     public int[] searchRange(int[] nums, int target) {
-        int a=leftMost(nums,target);
-        int b=rightMost(nums,target);
-        int ans[]={a,b};
-        
-        return ans;
-    }
-
-    public int leftMost(int nums[],int target){
-        int l=0;
-        int r=nums.length-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>=target)  r=mid-1;
-            else   l=mid+1;
-        }
-        if(l>=nums.length) return -1;
-        if(nums[l]!=target) return -1;
-        
-        return l;
-    }
-    public int rightMost(int nums[],int target){
-        int l=0;
-        int r=nums.length-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>target) r=mid-1;
-            else l=mid+1;
-        }
-        if(r<0) return -1;
-        if(nums[r]!=target) return -1;
-        return r;
+        int l=lowerBound(nums,target);
+        int u=upperBound(nums,target);
+        return new int[] {l,u};
     }
 }
