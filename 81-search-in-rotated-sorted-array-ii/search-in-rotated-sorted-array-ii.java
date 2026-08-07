4a835426308bd762fd6936a73838bc561d854fcb
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-08-07 21:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (66.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,15 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
-        int low=0,high=nums.length-1;
+       int low=0,high=nums.length-1;
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target) return true;
+
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low=low+1;
+                high=high-1;
+                continue;
+            }
 
             if(nums[low]<=nums[mid]){
                 if(nums[low]<=target && target<=nums[mid]) high=mid-1;
@@ -14,7 +19,6 @@ class Solution {
                 if(nums[mid]<=target && target<=nums[high]) low=mid+1;
                 else high=mid-1;
             }
-            
         }
         return false;
     }
