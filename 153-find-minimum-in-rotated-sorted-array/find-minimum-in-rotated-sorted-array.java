2fd696b10adc5b82bfa854a66ccd77424b38c5ad
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-08-09 15:50:00 +0530

    Time: 4 ms (5.12%) | Memory: 44.6 MB (8.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -15,10 +15,7 @@ class Solution {
         // }
         // return min;
 
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++){
-            min=Math.min(min,nums[i]);
-        }
-        return min;
+        Arrays.sort(nums);
+        return nums[0];
     }
 }
