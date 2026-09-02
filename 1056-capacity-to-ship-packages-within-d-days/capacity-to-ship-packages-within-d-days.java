392e1f65dab4dc9f54213aa608c8142d8bb773f8
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-09-02 19:57:38 +0530

    Time: 11 ms (64.97%) | Memory: 50.4 MB (19.82%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,26 +1,31 @@
 class Solution {
-    public boolean isPossible(int[] nums,int days,int maxCapacity){
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>maxCapacity) return false;
-            if(sum+nums[i]>maxCapacity){
-                days-=1;
-                sum=0;
+    public int fun(int a[],int days,int cap){
+        int day=1,load=0;
+        for(int j=0;j<a.length;j++){
+            if(load+a[j]>cap){
+                day++;
+                load=a[j];
             }
-            
-            sum+=nums[i];
-            if(days<=0) return false;
+            else{
+                load+=a[j];
+            }
         }
-        return true;
+        return day;
     }
     public int shipWithinDays(int[] weights, int days) {
-        int l=1;
-        int r=Integer.MAX_VALUE;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(isPossible(weights,days,mid)) r=mid-1;
-            else l=mid+1;
+        int max=0,sum=0;
+        for(int i=0;i<weights.length;i++){
+            if(weights[i]>max) max=weights[i];
+            sum+=weights[i];
         }
-        return l;
+
+        int low=max,high=sum;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int daysReq=fun(weights,days,mid);
+            if(daysReq<=days) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
     }
 }
