894a27244d14791a3bdb376961ffd0a7450d94a1
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-09-10 21:32:58 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (51.69%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,32 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> al=new ArrayList<>();
-        int i=0,j=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<nums2[j]) {
-                al.add(nums1[i]);
-                i++;
+        if(nums2.length < nums1.length) return findMedianSortedArrays(nums2,nums1);
+
+        int n1=nums1.length;
+        int n2=nums2.length;
+
+        int low=0,high=n1;
+
+        while(low<=high){
+            int cut1=(low+high)/2;
+            int cut2=(n1+n2+1)/2 - cut1;
+
+            int left1= (cut1==0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int left2= (cut2==0) ? Integer.MIN_VALUE : nums2[cut2-1];
+
+            int right1= (cut1==n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int right2= (cut2==n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if(left1 <= right2 && left2 <= right1){
+                if((n1+n2)%2==0){
+                    return (Math.max(left1,left2) + Math.min(right1,right2))/2.0;
+                }
+                else{
+                    return (Math.max(left1,left2));
+                }
             }
-            else{
-                al.add(nums2[j]);
-                j++;
-            }
+            else if(left1 > right2) high=cut1-1;
+            else low=cut1+1;
         }
-        while(i<nums1.length){
-            al.add(nums1[i]);
-            i++;
-        }
-        while(j<nums2.length){
-            al.add(nums2[j]);
-            j++;
-        }
-        int m1,m2;
-        int len=al.size();
-        if(al.size()%2==0){
-            return (al.get(len/2) + al.get(len/2-1))/2.0;
-        }
-        else{
-            return (double) al.get(len/2);
-        }
+
+        return 0.0;
     }
 }
