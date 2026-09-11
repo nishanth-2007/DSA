890712d06557f7ecdc0f752c27f83c6600ca799f
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-09-11 22:42:08 +0530

    Time: 5 ms (14.03%) | Memory: 48.4 MB (10.11%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,12 +1,26 @@
 class Solution {
+    public boolean bs(int matrix[],int target){
+        int low=0,high=matrix.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(matrix[mid]==target) return true;
+            else if(matrix[mid]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return false;
+    }
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length,n=matrix[0].length;
-        int i=0,j=n-1;
-        while(i<m && j>=0){
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
-            else i++;
+        // int i=0,j=n-1;
+        // while(i<m && j>=0){
+        //     if(matrix[i][j]==target) return true;
+        //     else if(matrix[i][j]>target) j--;
+        //     else i++;
+        // }
+        for(int i=0;i<m;i++){
+            if(bs(matrix[i],target)) return true;
         }
+
         return false;
     }
 }
