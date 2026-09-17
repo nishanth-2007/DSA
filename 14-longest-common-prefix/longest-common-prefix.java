be21f447bcce93882cae536749a56a5f1963cd4c
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-09-17 20:05:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (32.33%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -2,11 +2,10 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length==0) return "";
 
-        for(int i=0;i<strs[0].length();i++){
-            char ch=strs[0].charAt(i);
-            for(int j=1;j<strs.length;j++){
-                if(i>=strs[j].length() || ch!=strs[j].charAt(i)) return strs[0].substring(0,i);
-            }
+        Arrays.sort(strs);
+        String str1=strs[0],str2=strs[strs.length-1];
+        for(int i=0;i< Math.min(str1.length(),str2.length());i++){
+            if(str1.charAt(i)!=str2.charAt(i)) return str1.substring(0,i) ;
         }
         return strs[0];
     }
