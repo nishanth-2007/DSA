c4192ec896a5d7f4db3fc540011003df8f015b13
GODASU NISHANTH <godasunishanth04@gmail.com>
2026-09-19 18:55:24 +0530

    Time: 19 ms (40.80%) | Memory: 47.1 MB (13.68%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,12 +1,10 @@
 class Solution {
-    public static boolean isPalindrome(String str){
-        int l=0,r=str.length()-1;
-        while(l < r){
-            if(str.charAt(l) != str.charAt(r)) return false;
-            l++;
-            r--;
+    public String expandString(String str,int l,int r){
+        while(l>=0 && r<str.length() && str.charAt(l)==str.charAt(r)){
+            l--;
+            r++;
         }
-        return true;
+        return str.substring(l+1,r);
     }
     public String longestPalindrome(String s) {
         if(s.length()<=1) return s;
@@ -15,11 +13,15 @@ class Solution {
         String maxPalindrome=s.substring(0,1);
 
         for(int i=0;i<s.length();i++){
-            for(int j=i+maxLen;j<=s.length();j++){
-                if(j-i>maxLen && isPalindrome(s.substring(i,j))){
-                    maxLen=j-i;
-                    maxPalindrome=s.substring(i,j);
-                }
+            String odd=expandString(s,i,i);
+            String even=expandString(s,i,i+1);
+
+            if(odd.length()>maxPalindrome.length()){
+                maxPalindrome=odd;
+            }
+
+            if(even.length()>maxPalindrome.length()){
+                maxPalindrome=even;
             }
         }
 
